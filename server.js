38c3bd538b6aae87bb5fef18e4ad153f27de6c56
viva <vivekaloganathan37@gmail.com>
2020-08-20 22:04:53 +0530

    Updated with heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/event-frontend'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/event-frontend/index.html'));
+});
+app.listen(process.env.PORT || 8080);
