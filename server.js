7e0ba4d7d64825c87f0d893e571cf965f707e64f
viva <vivekaloganathan37@gmail.com>
2020-08-21 09:28:26 +0530

    Updated with the server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/event-frontend'));
+app.use(express.static(__dirname + '/dist/heroku-test'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/event-frontend/index.html'));
+        '/dist/heroku-test/index.html'));
 });
 app.listen(process.env.PORT || 8080);
